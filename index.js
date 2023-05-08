844e5b507f33ee0cbd47bba63ac5a1a85899d64d
isnancahyadi <isnan.arifc@gmail.com>
2023-05-08 11:37:38 +0700

    Get all users

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // routes
+// users
+app.get('/user', async function (req, res) {
+    const query = await db`SELECT * FROM users`;
+
+    res.json({
+        status: true,
+        message: "Get data success",
+        data: query
+    })
+})
+
+// root
 app.get('/', function (req, res) {
     res.send('Hello World');
 });

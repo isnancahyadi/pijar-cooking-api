60033dfce01ffe93fd67a5f1a669fefff58945fb
isnancahyadi <isnan.arifc@gmail.com>
2023-06-06 17:25:21 +0700

    [FIX] send JSON type to object

--- response.js
+++ response.js
@@ -1,11 +1,9 @@
 const response = (statusCode, stat, message, payLoad, res) => {
-    res.status(statusCode).json([
-        {
-            status: stat,
-            message,
-            ...payLoad && {payload : payLoad}
-        }
-    ]);
+  res.status(statusCode).json({
+    status: stat,
+    message,
+    ...(payLoad && { payload: payLoad }),
+  });
 };
 
 module.exports = response;
